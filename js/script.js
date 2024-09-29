d8984fc3e6f12332be0a4e724b49d901ee65b7b6
imonrana <info.imon@yahoo.com>
2024-09-30 04:17:07 +0600

    js multipication

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// namta
+
+let namta = prompt( "Please Enter Multiply Number: " + "");
+nam = parseInt(namta)
+for(let i = 1; i<=10; i++){
+    let result = `${nam} + ${i} = ${nam + i} <br> `;
+    document.write(result);
+}
