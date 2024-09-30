2fe3b2de85281208ac66abc5e8e3ceedaacb69cb
imonrana <info.imon@yahoo.com>
2024-09-30 11:55:58 +0600

    update js file

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 // namta
 
-let namta = prompt( "Please Enter Multiply Number: " + "");
+let namta = prompt( "Please Enter Multiply Number:" + "");
 nam = parseInt(namta)
 for(let i = 1; i<=10; i++){
-    let result = `${nam} + ${i} = ${nam + i} <br> `;
+    let result = `${nam} x ${i} = ${nam * i} <br> `;
     document.write(result);
 }
